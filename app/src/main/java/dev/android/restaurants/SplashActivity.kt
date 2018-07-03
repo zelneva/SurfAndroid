8348799a02e9bf8bfbeeb0dcecb24f4ee87e01bb
Anastasia Zelneva <anastasia.vrn97@gmail.com>
2018-07-03 17:46:22 +0300

    Initial commit

---
+++ app/src/main/java/dev/android/restaurants/SplashActivity.kt
@@ -0,0 +1,17 @@
+package dev.android.restaurants
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.content.Intent
+
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
